a91e2160639a6126da125d7e519d371cea077d44
Stephen Rado <stejay@thirteen37.za.net>
2017-05-11 13:44:02 +0100

    Last commit

--- js/test.js
+++ js/test.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
     $(".js-sidebar").click(function () {
-        $('.main').toggleClass("sidebar");
+        console.log($(".sidebar").width());
+        if ($(".sidebar").width() > 1){
+        	$(".sidebar").animate({ width:"0"}, 1500).hide(50);
+        	$(".content").animate({ width:"100%"}, 1500);
+        }else{
+        	$(".sidebar").animate({ width:"30%"}, 1500).show();
+        	$(".content").animate({ width:"70%"}, 1500);
+        }
     });
 });
